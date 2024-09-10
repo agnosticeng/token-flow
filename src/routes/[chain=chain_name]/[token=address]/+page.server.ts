09d9d1ac22d9282377789b2cf9df9e33de731631
Yann Amsellem <yann@agnostic.engineering>
2024-09-10 21:26:41 +0200

    chore: get token details

--- src/routes/[chain=chain_name]/[token=address]/+page.server.ts
+++ src/routes/[chain=chain_name]/[token=address]/+page.server.ts
@@ -32,7 +32,10 @@ export const load = (async (e) => {
 			body: body<QueryVariables>(query, { token: e.params.token })
 		}).then((r) => r.json<QueryResponse>());
 
-		return { holders: transform(response) };
+		return {
+			holders: transform(response),
+			token: await getTokenInfo(e.params.chain, e.params.token)
+		};
 	} catch (e) {
 		console.error(e);
 		error(404);
@@ -51,4 +54,52 @@ function transform({ data }: QueryResponse) {
 		amount: parseFloat(d.amount),
 		percent: parseFloat(d.percent)
 	}));
+}
+
+async function getTokenInfo(chain: string, address: `0x${string}`) {
+	const query = /* GraphQL */ `
+		query Token($input: TokenInput!) {
+			token(input: $input) {
+				token {
+					id
+					address
+					name
+					symbol
+					decimals
+					logoURI
+				}
+			}
+		}
+	`;
+
+	const { data } = await fetch('https://assets.agnostic.dev/graphql', {
+		method: 'POST',
+		headers: { 'Content-type': 'application/json' },
+		body: JSON.stringify({
+			query,
+			variables: { input: { address, chain: capitalize(chain) } },
+			operationName: 'Token'
+		})
+	}).then((r) =>
+		r.json<{
+			data: {
+				token: {
+					token: {
+						id: string;
+						address: string;
+						name: string;
+						symbol: string;
+						decimals: string;
+						logoURI: string;
+					};
+				};
+			};
+		}>()
+	);
+
+	return data.token.token;
+}
+
+function capitalize(s: string) {
+	return s.slice(0, 1).toUpperCase() + s.slice(1);
 }
--- src/routes/[chain=chain_name]/[token=address]/+page.svelte
+++ src/routes/[chain=chain_name]/[token=address]/+page.svelte
@@ -8,6 +8,16 @@
 	let width: number;
 </script>
 
+<svelte:head>
+	<title>{data.token.symbol} TokenFlow</title>
+</svelte:head>
+
+<header>
+	<div>
+		<img src={data.token.logoURI} alt="{data.token.name} 's Icon" />
+		<span data-kind="headline/h3">{data.token.name}</span>
+	</div>
+</header>
 <section bind:clientWidth={width} bind:clientHeight={height}>
 	{#if height && width}
 		<Graph holders={data.holders} {height} {width} />
@@ -15,8 +25,39 @@
 </section>
 
 <style>
+	header {
+		height: 64px;
+		width: 100%;
+		background-color: hsl(270deg 14% 12%);
+		position: relative;
+		z-index: 1;
+		padding: 0 64px;
+
+		@media screen and (max-width: 768px) {
+			padding: 0 32px;
+		}
+
+		@media screen and (max-width: 576px) {
+			padding: 0 16px;
+		}
+
+		& > div {
+			height: 100%;
+
+			display: flex;
+			align-items: center;
+			gap: 14px;
+
+			& > img {
+				width: 24px;
+				height: 24px;
+				border-radius: 50%;
+			}
+		}
+	}
+
 	section {
-		height: 100%;
+		height: calc(100% - 64px);
 		width: 100%;
 
 		position: relative;
